7aa5b7108102aaaae7c0ff2dcf92ce07e49e15c1
David Dixon <david@digitaloasis.co.uk>
2018-06-21 12:54:14 +0100

    sha256 update to match current 0.1.4

--- assume-role.rb
+++ assume-role.rb
@@ -3,7 +3,7 @@ require 'formula'
 class AssumeRole < Formula
   homepage 'https://github.com/travelex/assume-role'
   url 'https://github.com/travelex/assume-role/archive/v0.1.4.tar.gz'
-  sha256 '6ab3af1bdf4099b6f8856071183a1eaa2ae722cd2f6d39d1162b4d1eea7e8acc'
+  sha256 'a4fb7b9dfc2ec5798b164ee27fbc4a71fb8d83505e14b55532b596567a718e94'
 
   depends_on "awscli"
   depends_on "jq"
