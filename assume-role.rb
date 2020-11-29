353d4405950c1909d47eef3fef02e426da7c235a
David Dixon <david.dixon@konsentus.com>
2020-11-29 01:21:24 +0000

    new release

--- assume-role.rb
+++ assume-role.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class AssumeRole < Formula
   homepage 'https://github.com/davidjdixon/assume-role'
-  url 'https://github.com/davidjdixon/assume-role/archive/v0.3.0a.tar.gz'
-  sha256 '5f04015f9cbcf7efbc94637d7e25d15ec5a4003126a45c5ee532a592a5bcca75'
+  url 'https://github.com/davidjdixon/assume-role/archive/v0.3.1.tar.gz'
+  sha256 '597df5a817a1deac63e59090b37f4de7c398f758812c3a9b3e67863a05147225'
 
   depends_on "awscli"
   depends_on "jq"
