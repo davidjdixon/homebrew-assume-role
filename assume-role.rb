44367f1378348bfb069cf913cfce07ee0b3c882e
David Dixon <david@bricklane.com>
2022-05-23 13:10:09 +0100

    updated to more closely follow current brew standards

--- assume-role.rb
+++ assume-role.rb
@@ -1,16 +1,14 @@
-require 'formula'
-
 class AssumeRole < Formula
-  desc "A CLI tool making it easy to assume IAM roles through an AWS Bastion account"
-  homepage 'https://github.com/davidjdixon/assume-role'
-  url 'https://github.com/davidjdixon/assume-role/archive/v0.3.2.tar.gz'
-  sha256 'aa05b3bdd38940581ef2abaae944bc47498933619e00cd0e34891f4ed58578c7'
+  desc "CLI tool making it easy to assume IAM roles through an AWS Bastion account"
+  homepage "https://github.com/davidjdixon/assume-role"
+  url "https://github.com/davidjdixon/assume-role/archive/v0.3.2.tar.gz"
+  sha256 "aa05b3bdd38940581ef2abaae944bc47498933619e00cd0e34891f4ed58578c7"
   license "Apache-2.0"
 
   depends_on "awscli"
   depends_on "jq"
 
   def install
-    bin.install File.join('.', 'assume-role')
+    bin.install File.join(".", "assume-role")
   end
 end
