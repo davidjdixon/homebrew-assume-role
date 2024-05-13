74a76421803d51b57d55afe514e7a6bc8e82c5e3
David Dixon <david@bricklane.com>
2024-05-13 18:54:08 +0100

    v0.4

--- assume-role.rb
+++ assume-role.rb
@@ -1,8 +1,8 @@
 class AssumeRole < Formula
   desc "CLI tool making it easy to assume IAM roles through an AWS Bastion account"
   homepage "https://github.com/davidjdixon/assume-role"
-  url "https://github.com/davidjdixon/assume-role/archive/v0.3.3.tar.gz"
-  sha256 "7f83a35a5ed9e1a016f02f70802c962838d1e41ba0917b30eadedef106f2fcf1"
+  url "https://github.com/davidjdixon/assume-role/archive/v0.4.tar.gz"
+  sha256 "fc62b015b1755b38e485ae1d8e78cd9b89ab22f0c88f60ec6bee75d9f8d897df"
   license "Apache-2.0"
 
   depends_on "jq"
